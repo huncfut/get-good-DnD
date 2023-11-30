a08e4d045c5a9f12a2e997cc41b64de61db2d06c
Harrison <hhhoytt@gmail.com>
2023-11-29 19:00:07 -0500

    Added subsampling to decision tree

--- decision-tree.py
+++ decision-tree.py
@@ -1,27 +1,35 @@
 import pandas as pd
 import numpy as np
+import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 
-stats = ["s_str", "s_dex", "s_con", "s_int", "s_wis", "s_cha"]
-data = pd.read_csv("./data/dndData.csv")
-X_train, X_test, y_train, y_test = train_test_split(np.array(data[stats], dtype='int'), np.array(data["class_starting"]), random_state=2137, test_size=0.2)
+warnings.filterwarnings("ignore")
 
-clf = tree.DecisionTreeClassifier(max_depth=29)
+stats = ["s_str", "s_dex", "s_con", "s_int", "s_wis", "s_cha"]
+stats_names = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charmisma"]
+data = pd.read_csv("./data/proportionalData.csv")
+
+downsized_stats = pd.DataFrame(columns=["s_str", "s_dex", "s_con", "s_int", "s_wis", "s_cha", "class_starting"])
+for c in data["class_starting"].unique():
+    sample = data[data["class_starting"] == c].sample(n=40000, random_state=2137, replace=True)
+    downsized_stats = downsized_stats.append(sample)
+
+X_train, X_test, y_train, y_test = train_test_split(np.array(downsized_stats[stats]), np.array(downsized_stats["class_starting"]), random_state=2137, test_size=0.2)
+
+clf = tree.DecisionTreeClassifier(max_depth=28)
 clf.fit(X_train, y_train)
 y_predict = clf.predict(X_train)
 train_acc = accuracy_score(y_train, y_predict)
 y_predict = clf.predict(X_test)
 test_acc = accuracy_score(y_test, y_predict)
 
+print("Train Accuracy: " + str(train_acc) + " Test Accuracy: " + str(test_acc))
+
 vals = []
-vals.append(input("Enter Strength: "))
-vals.append(input("Enter Dexterity: "))
-vals.append(input("Enter Constitution: "))
-vals.append(input("Enter Intelligence: "))
-vals.append(input("Enter Wisdom: "))
-vals.append(input("Enter Charmisma: "))
+for s in stats_names:
+    vals.append(float(input("Enter " + s + ": "))/18)
 
 val_acc = clf.predict([vals])
 print("Best Class: ", val_acc[0])
