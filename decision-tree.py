031a52aeaa2cf88505aa0f6bbeb1f3da7d575758
Harrison <hhhoytt@gmail.com>
2023-12-08 23:02:27 -0500

    Final push

--- decision-tree.py
+++ decision-tree.py
@@ -9,11 +9,11 @@ warnings.filterwarnings("ignore")
 
 stats = ["s_str", "s_dex", "s_con", "s_int", "s_wis", "s_cha"]
 stats_names = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charmisma"]
-data = pd.read_csv("./data/proportionalData.csv")
+data = pd.read_csv("./data/proportionalDataStripped.csv")
 
 downsized_stats = pd.DataFrame(columns=["s_str", "s_dex", "s_con", "s_int", "s_wis", "s_cha", "class_starting"])
 for c in data["class_starting"].unique():
-    sample = data[data["class_starting"] == c].sample(n=40000, random_state=2137, replace=True)
+    sample = data[data["class_starting"] == c].sample(n=50000, random_state=2137, replace=True)
     downsized_stats = downsized_stats.append(sample)
 
 X_train, X_test, y_train, y_test = train_test_split(np.array(downsized_stats[stats]), np.array(downsized_stats["class_starting"]), random_state=2137, test_size=0.2)
@@ -29,7 +29,7 @@ print("Train Accuracy: " + str(train_acc) + " Test Accuracy: " + str(test_acc))
 
 vals = []
 for s in stats_names:
-    vals.append(float(input("Enter " + s + ": "))/18)
+    vals.append(int(input("Enter " + s + ": ")))
 
-val_acc = clf.predict([vals])
+val_acc = clf.predict([vals / np.max(vals)])
 print("Best Class: ", val_acc[0])
